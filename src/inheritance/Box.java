553a6de3546b7ff1e5d08008227173bf5fbcef68
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-11 12:42:39 -0400

    Revised Inheritance

---
+++ src/inheritance/Box.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+public class Box {
+    double l;
+    double w;
+    double h;
+
+     Box (){
+         this.l=-20;
+         this.w=-20;
+         this.h=-10;
+    }
+    Box(double side){
+         this.l=side;
+         this.h=side;
+         this.w=side;
+
+    }
+    Box(double l,double w,double h){
+         this.l=l;
+         this.h=h;
+         this.w=w;
+    }
+}
