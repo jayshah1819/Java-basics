4978ac46f97e7204318084b3047ffe10ec92275b
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-12 17:14:55 -0400

    Solved Errors

--- src/Polymorphism/CompileTimePoly/Intro2.java
+++ src/Polymorphism/CompileTimePoly/Intro2.java
@@ -8,7 +8,12 @@ public class Intro2 extends Intro1{
         super(erNo);
         this.Password=Password;
     }
-    Intro2(String degreeType ,String Name, String LastName){
-        Super(degreeType,Name,LastName);
+    Intro2(String degreeType ,String Name, String LastName,String UserName){
+        super(degreeType,Name,LastName);
+        this.UserName=UserName;
+    }
+    Intro2(String email, String Password){
+        this.Email=email;
+        this.Password=Password;
     }
 }
