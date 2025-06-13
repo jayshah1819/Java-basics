b14749eafbf0782977f3a7c453891786a010a42e
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-13 15:27:37 -0400

    updated

--- src/Polymorphism/shapes.java
+++ src/Polymorphism/shapes.java
@@ -3,5 +3,6 @@ package Polymorphism;
 public class shapes {
     static void sides(){
         System.out.println("Every shape will have different types of sides");
+        System.out.println("This will work as side so we will check static");
     }
 }
