56c613a4b909cbae0886e9fdeb8050073bf7504b
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-14 11:19:41 -0400

    Update

--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Box box1=new Box();
         BoxWeight box2=new BoxWeight(1,3,3);
         System.out.println(box2.l+" "+box2.h+" "+box2.w+" "+box2.weight);
-
+        //just checked and added new values.
         Box box3=new BoxWeight(1,3,4);
     }
 
