530fa476d1abd9ea08b2f6cfb45d1d0e4eb1b24a
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-15 15:41:30 -0400

    Update BoxWeight.java

--- src/inheritance/BoxWeight.java
+++ src/inheritance/BoxWeight.java
@@ -5,5 +5,6 @@ public class BoxWeight extends Box {
     BoxWeight(double l,double h,double w ){
         super(l,h,w);
         this.weight=-1;
+        //super: to get it from parent class
     }
 }
