7619ecfeb539d8a45857c7f413ed1a2ba3cf2df9
jayshah1819 <jayshah.jk.jk18@gmail.com>
2025-06-18 16:23:08 -0400

    Update Bank.java

--- src/OOPsConcecpts/ProjcectUsingOops/Bank.java
+++ src/OOPsConcecpts/ProjcectUsingOops/Bank.java
@@ -1,5 +1,30 @@
 package OOPsConcecpts.ProjcectUsingOops;
 
+import java.util.Scanner;
+
 public class Bank {
+    public static void main(String[] args){
+        User user1=new User();
+
+        Scanner myobj=new Scanner(System.in);
+        System.out.println("Please Write your name");
+        String userName=myobj.nextLine();
+        System.out.println("Nice to meet you"+ userName);
+        System.out.println("Can you type your Account Number associated with us or press 1 to open new account"+userName);
+        int accountNumber=myobj.nextInt();
+        if(accountNumber==1){
+            user1.AccountNumber=accountNumber;
+        }
+        else{
+
+        }
+
+
+
+
+        user1.name=userName;
+
+
+    }
 
 }
